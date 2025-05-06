076e8c27c2a6f430085a9f68a261d94934a26ac7
That-LittleBird <alexia.daniela07@gmail.com>
2025-05-06 17:28:44 -0400

    Yay

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,22 @@
-console.log("Hello World");
+const personne = document.querySelectorAll('.personne');
+const modale = document.querySelector('.modale');
+
+for (let i = 0; i < personne.length; i++){
+    personne[i].addEventListener('click', openModale);
+}
+
+function openModale(){
+    const chemin = this.getAttribute("src");
+    const image = document.createElement('img');
+    image.setAttribute('src', chemin);
+    image.classList.add('zoom');
+    modale.appendChild(image);
+    modale.classList.add('visible');
+    image.addEventListener("click", closeModale);
+    console.log("hello");
+}
+
+function closeModale(event) {
+    modale.removeChild(event.target);
+    modale.classList.remove('visible');
+}
