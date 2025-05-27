7cafa63dc810a94fc9b92e0781201e058b947e67
That-LittleBird <alexia.daniela07@gmail.com>
2025-05-27 17:32:00 -0400

    Last

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,49 @@
-const personne = document.querySelectorAll('.personne');
-const modale = document.querySelector('.modale');
+const personne = document.querySelectorAll(".personne");
+const modale = document.querySelector(".modale");
+const arriere = document.querySelector(".modaleBoîte")
 
 for (let i = 0; i < personne.length; i++){
     personne[i].addEventListener('click', openModale);
 }
 
 function openModale(){
+    const parent = this.closest("article");
+
     const source = this.getElementsByTagName("img");
     const chemin = source[0].getAttribute("src");
-
-    const source1 = this.getElementsByTagName("h3");
-    const chemin1 = source1[0].getAttribute("src");
-
-    const source2 = this.getElementsByTagName("p");
-    const chemin2 = source2[0].getAttribute("src");
-
     const image = document.createElement("img");
-    const nom = document.createElement("textcontent");
-    const titre = document.createElement("textcontent");
-
-
     image.setAttribute('src', chemin);
-
     image.classList.add('zoom');
     modale.appendChild(image);
-    modale.classList.add('visible');
 
+    const nomContent = parent.querySelector("h3").textContent;
+    const nom = document.createElement("h3");
+    nom.textContent = nomContent;
+    nom.classList.add("nom");
+    modale.appendChild(nom);
 
+    const titreContent = parent.querySelector(".titre").textContent;
+    const titre = document.createElement("p");
+    titre.textContent = titreContent;
+    titre.classList.add("titre");
+    modale.appendChild(titre);
+
+    arriere.classList.add('visible');
     modale.addEventListener("click", closeModale);
 }
 
-function closeModale(event) {
-    modale.removeChild(event.target);
-    modale.classList.remove('visible');
+function closeModale(event){
+    const nom = document.querySelector(".nom");
+    const titre = document.querySelector(".titre")
+    const image = document.querySelector(".zoom");
+
+    nom.remove();
+    titre.remove();
+    image.remove();
+
+    nom.classList.remove("nom");
+    titre.classList.remove("titre");
+    image.classList.remove("zoom");
+
+    arriere.classList.remove('visible');
 }
